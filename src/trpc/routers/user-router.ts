8dca9d3fa002f823cd55d2ed373a7435c276e32d
Eduardo Isidoro <eisilva1496@gmail.com>
2025-02-02 20:18:50 -0300

    feat: add getUserAlbums procedure to user router

--- src/trpc/routers/user-router.ts
+++ src/trpc/routers/user-router.ts
@@ -1,19 +1,39 @@
+import { z } from "zod";
+import { TRPCError } from "@trpc/server";
 import { env } from "@/config/env";
 import { createTRPCRouter, baseProcedure } from "@/trpc/init";
-
+import { Album } from "@/types/album";
 import { User } from "@/types/user";
-import { TRPCError } from "@trpc/server";
 
 export const userRouter = createTRPCRouter({
-  getUsers: baseProcedure.query(async ({ ctx }) => {
+  getUsers: baseProcedure.query(async () => {
     try {
       const response = await fetch(env.NEXT_PUBLIC_USERS_API);
       const data = await response.json();
       return data as User[];
     } catch (error) {
+      console.error("TRPC: Failed to fetch users", error);
+
       if (error instanceof TRPCError) {
         throw error;
       }
     }
   }),
+
+  getUserAlbums: baseProcedure
+    .input(z.object({ userId: z.string() }))
+    .query(async ({ input }) => {
+      try {
+        const response = await fetch(
+          `${env.NEXT_PUBLIC_USERS_API}/${input.userId}/albums`
+        );
+        const data = await response.json();
+        return data as Album[];
+      } catch (error) {
+        console.error("TRPC: Failed to fetch users", error);
+        if (error instanceof TRPCError) {
+          throw error;
+        }
+      }
+    }),
 });
