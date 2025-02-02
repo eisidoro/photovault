3c1513ec44f32bbe6e17ca31b21d959236c8808a
Eduardo Isidoro <eisilva1496@gmail.com>
2025-02-02 19:58:40 -0300

    feat: add NEXT_PUBLIC_USERS_API environment variable

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,9 +3,11 @@ import { z } from "zod";
 const envSchema = z.object({
   NEXT_PUBLIC_SUPABASE_URL: z.string(),
   NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
+  NEXT_PUBLIC_USERS_API: z.string(),
 });
 
 export const env = envSchema.parse({
   NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
   NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  NEXT_PUBLIC_USERS_API: process.env.NEXT_PUBLIC_USERS_API,
 });
