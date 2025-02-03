d14ff5ee61a7a43a2a2cb2b05a39a71ef3dab575
Eduardo Isidoro <eisilva1496@gmail.com>
2025-02-03 10:13:14 -0300

    refactor: update AlbumsList component with dynamic loading and improved layout

--- src/components/albums/albums-list/albums-list.tsx
+++ src/components/albums/albums-list/albums-list.tsx
@@ -1,26 +1,31 @@
+import Link from "next/link";
+import { useParams } from "next/navigation";
 import { useUserAlbums } from "@/hooks/use-user-albums";
 import { AlbumsListItem } from "../albums-list-item";
+import { Loader2 } from "lucide-react";
 import styles from "./albums-list.module.css";
-import Link from "next/link";
 
-interface AlbumsListProps {
-  userId: string;
-}
-
-export const AlbumsList = ({ userId }: AlbumsListProps) => {
-  const { albums, isLoading } = useUserAlbums(userId);
+export function AlbumsList() {
+  const { userId } = useParams();
+  const { albums, isLoading } = useUserAlbums(userId as string);
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className={styles["albums-list__loading"]}>
+        <Loader2 className="w-10 h-10 animate-spin" />
+      </div>
+    );
   }
 
   return (
     <div className={styles["albums-list__container"]}>
-      {albums?.map((album) => (
-        <Link href={`/user/${userId}/albums/${album.id}`} key={album.id}>
-          <AlbumsListItem album={album} />
-        </Link>
-      ))}
+      <div className={styles["albums-list__content"]}>
+        {albums?.map((album) => (
+          <Link href={`/user/${userId}/albums/${album.id}`} key={album.id}>
+            <AlbumsListItem album={album} />
+          </Link>
+        ))}
+      </div>
     </div>
   );
-};
+}
