eb89cab941b29f1102a4f1f18e29cfebd30c5b97
Eduardo Isidoro <eisilva1496@gmail.com>
2025-02-03 11:41:39 -0300

    feat: integrate useUserAlbums hook in Albums page component

--- src/sections/albums/albums.tsx
+++ src/sections/albums/albums.tsx
@@ -2,14 +2,21 @@ import { AlbumsList } from "@/components/albums/albums-list";
 import { useUsers } from "@/hooks/use-users";
 import { useParams } from "next/navigation";
 import { PageHeader } from "@/components/common/page-header";
+import { useUserAlbums } from "@/hooks/use-user-albums";
 
 export function Albums() {
   const { userId } = useParams();
   const { currentUser } = useUsers({ userId: userId as string });
+  const { albums, isLoading } = useUserAlbums(userId as string);
+
   return (
     <>
       <PageHeader title={`${currentUser?.name} > Albums`} backButton />
-      <AlbumsList />
+      <AlbumsList
+        albums={albums ?? []}
+        isLoading={isLoading}
+        userId={userId as string}
+      />
     </>
   );
 }
