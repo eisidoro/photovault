312b7998241d7e0632634f83399555c09d7608f4
Eduardo Isidoro <eisilva1496@gmail.com>
2025-02-03 10:15:52 -0300

    refactor: replace inline login form with Login component

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,5 @@
-import { login, signup } from "./actions";
+import { Login } from "@/sections/auth/login";
 
 export default function LoginPage() {
-  return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
-  );
+  return <Login />;
 }
