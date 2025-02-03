e2a8dd217dac59eb36e782e9be4ba3c9d8bbfc72
Eduardo Isidoro <eisilva1496@gmail.com>
2025-02-03 11:38:48 -0300

    refactor: update tRPC context and app router to remove hardcoded user ID

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,10 @@
 import { createTRPCRouter } from "../init";
 import { userRouter } from "./user-router";
+import { albumRouter } from "./album-router";
 
 export const appRouter = createTRPCRouter({
   user: userRouter,
+  albums: albumRouter,
 });
 
 export type AppRouter = typeof appRouter;
