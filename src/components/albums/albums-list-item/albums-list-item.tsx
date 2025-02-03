d420993d910514e7e1ecef5dae26c995bfc97251
Eduardo Isidoro <eisilva1496@gmail.com>
2025-02-02 22:35:42 -0300

    refactor: update AlbumsListItem component styling and structure

--- src/components/albums/albums-list-item/albums-list-item.tsx
+++ src/components/albums/albums-list-item/albums-list-item.tsx
@@ -1,13 +1,12 @@
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-
 import { Album } from "@/types/album";
+import styles from "./albums-list-item.module.css";
 
 interface AlbumsListItemProps {
   album: Album;
@@ -15,16 +14,13 @@ interface AlbumsListItemProps {
 
 export const AlbumsListItem = ({ album }: AlbumsListItemProps) => {
   return (
-    <Card>
+    <Card className={styles["albums-list-item__container"]}>
       <CardHeader>
         <CardTitle>{album.title}</CardTitle>
       </CardHeader>
       <CardContent>
         <p>{album.title}</p>
       </CardContent>
-      <CardFooter>
-        <p>{album.title}</p>
-      </CardFooter>
     </Card>
   );
 };
