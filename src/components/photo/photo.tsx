b82e1b0eb6430822e60d845ab664c8997f63006c
Eduardo Isidoro <eisilva1496@gmail.com>
2025-02-03 07:55:37 -0300

    feat: improve Photo component styling and error handling

--- src/components/photo/photo.tsx
+++ src/components/photo/photo.tsx
@@ -1,16 +1,27 @@
-import { Photo as PhotoType } from "@/types/photo";
+import { useState } from "react";
 import Image from "next/image";
+import { Photo as PhotoType } from "@/types/photo";
 import { CardContent, CardHeader, CardTitle } from "../ui/card";
 import { Card } from "../ui/card";
 import styles from "./photo.module.css";
+
 export const Photo = ({ photo }: { photo: PhotoType }) => {
   return (
     <Card className={styles["photo__container"]}>
-      <CardContent>
-        <Image src={photo.url} alt={photo.title} width={100} height={100} />
+      <CardContent className={styles["photo__content"]}>
+        <Image
+          src={"https://dummyimage.com/300"}
+          alt={photo.title}
+          width={100}
+          height={100}
+          onError={(ev) => {
+            ev.currentTarget.onerror = null;
+            ev.currentTarget.src = "https://dummyimage.com/300";
+          }}
+        />
       </CardContent>
-      <CardHeader>
-        <CardTitle>{photo.title}</CardTitle>
+      <CardHeader className={styles["photo__header"]}>
+        <CardTitle className={styles["photo__title"]}>{photo.title}</CardTitle>
       </CardHeader>
     </Card>
   );
