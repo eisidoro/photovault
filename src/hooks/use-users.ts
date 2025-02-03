da7746001fca996a5057c8bdefc96a5747829c29
Eduardo Isidoro <eisilva1496@gmail.com>
2025-02-03 10:15:24 -0300

    feat: add currentUser retrieval to useUsers hook

--- src/hooks/use-users.ts
+++ src/hooks/use-users.ts
@@ -2,11 +2,22 @@
 
 import { trpc } from "@/trpc/client";
 
-export const useUsers = () => {
-  const { data, isLoading } = trpc.user.getUsers.useQuery(undefined, {});
+interface UseUsersProps {
+  userId?: string;
+}
+
+export const useUsers = ({ userId }: UseUsersProps = {}) => {
+  const { data, isLoading } = trpc.user.getUsers.useQuery();
+
+  let currentUser = null;
+
+  if (userId) {
+    currentUser = data?.find((user) => user.id === Number(userId));
+  }
 
   return {
     users: data ?? [],
+    currentUser,
     isLoading,
   };
 };
