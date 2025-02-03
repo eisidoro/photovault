3acf8135e5df428e0edc0e87559699c8c0d03709
Eduardo Isidoro <eisilva1496@gmail.com>
2025-02-03 10:14:32 -0300

    feat: implement UsersListItem with enhanced user details and avatar

--- src/components/users/users-list-item/users-list-item.tsx
+++ src/components/users/users-list-item/users-list-item.tsx
@@ -1,31 +1,36 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { User } from "@/types/user";
+import styles from "./users-list-item.module.css";
 
 interface UsersListItemProps {
   user: User;
 }
 
 export const UsersListItem = ({ user }: UsersListItemProps) => {
+  const userAbbreviation = user.name
+    ?.match(/(\b\S)?/g)
+    ?.join("")
+    ?.match(/(^\S|\S$)?/g)
+    ?.join("")
+    ?.toUpperCase();
+
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>{user.name}</CardTitle>
-        <CardDescription>{user.email}</CardDescription>
+    <Card className={styles["users-list-item__container"]}>
+      <CardHeader className="p-4">
+        <CardTitle className="flex items-center gap-2">
+          <Avatar className={styles["users-list-item__avatar"]}>
+            <AvatarImage src="" />
+            <AvatarFallback>{userAbbreviation}</AvatarFallback>
+          </Avatar>
+          {user.name}
+        </CardTitle>
       </CardHeader>
       <CardContent>
-        <p>{user.address.city}</p>
+        <p>E-mail: {user.email.toLowerCase()}</p>
+        <p>Address: {user.address.city}</p>
+        <p>Phone: {user.phone}</p>
       </CardContent>
-      <CardFooter>
-        <p>{user.phone}</p>
-      </CardFooter>
     </Card>
   );
 };
