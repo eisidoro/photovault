d5dfb29cb7118a86143d4f2868c052bbfaf07ed8
Eduardo Isidoro <eisilva1496@gmail.com>
2025-02-03 07:55:29 -0300

    feat: enhance Album page with dynamic title and improved grid layout

--- src/sections/album/album.tsx
+++ src/sections/album/album.tsx
@@ -1,21 +1,35 @@
+import { useMemo } from "react";
 import { useParams } from "next/navigation";
-import { Photo } from "@/components/photo";
-import { Photo as PhotoType } from "@/types/photo";
 import { useAlbum } from "@/hooks/use-album";
+import { useUserAlbums } from "@/hooks/use-user-albums";
+import { Photo as PhotoType } from "@/types/photo";
+import { Photo } from "@/components/photo";
 import { AlbumLoading } from "./album-loading";
 import styles from "./album.module.css";
 
 export const Album = () => {
-  const { albumId } = useParams();
+  const { userId, albumId } = useParams();
   const { photos, isLoading } = useAlbum(albumId as string);
+  const { albums, isLoading: isLoadingAlbums } = useUserAlbums(
+    userId as string
+  );
 
-  if (isLoading) {
+  const currentAlbumTitle = useMemo(() => {
+    const album = albums?.find((album) => album.id === Number(albumId));
+    return album?.title;
+  }, [albums, albumId]);
+
+  if (isLoading || isLoadingAlbums) {
     return <AlbumLoading />;
   }
 
   return (
     <div>
-      <h1>Album</h1>
+      <h1 className={styles["album__title-container"]}>
+        <span className={styles["album__title-prefix"]}>Album </span>
+        <span> &gt; </span>
+        <span className={styles["album__title"]}> {currentAlbumTitle}</span>
+      </h1>
       <div className={styles["album__container"]}>
         {photos?.map((photo: PhotoType) => (
           <Photo key={photo.id} photo={photo} />
